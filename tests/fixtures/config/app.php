7563fac94c3e62f730fb641cec951cef5ac37a40
Jukka <aannppaall@gmail.com>
2020-10-02 19:53:04 +0300

    Update app.php
    Fix for proper OctoberEnvTest passing

--- tests/fixtures/config/app.php
+++ tests/fixtures/config/app.php
@@ -2,8 +2,8 @@
 // Fixture used for `october:env` unit tests in `tests/unit/system/console/OctoberEnvTest.php
 
 return [
-    'debug' => true,
-    'url' => 'https://localhost',
+    'debug' => env('APP_DEBUG', true),
+    'url' => env('APP_URL', 'https://localhost'),
     'key' => 'CHANGE_ME!!!!!!!!!!!!!!!!!!!!!!!',
     'timezone' => 'UTC',
 ];
